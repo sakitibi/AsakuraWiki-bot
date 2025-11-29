b9b083479074b0cb39346efefc72b5f2056682dc
sakitibi <sakitibi10nin@icloud.com>
2025-11-29 13:37:47 +0900

    Merge remote changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const { createClient } = require("@supabase/supabase-js");
 const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
 
 // ▼ Discord Bot
