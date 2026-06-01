63d60ffca2f8139f6806ab04d489b52185015e89
sakitibi <sakitibi10nin@icloud.com>
2026-06-02 00:06:00 +0900

    Merge remote changes

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const loadNgPatterns = require("./ngwords")
 const listenSupabaseChange = require("./roomcode");
 const listenSupabaseChangeWikis = require("./wikis");
 require("dotenv").config({
-  path: "/Applications/discord_bot/.env"
+    path: "/Applications/discord_bot/.env"
 });
 
-// ▼ Discord Bot
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const juusanninTermsURL = "https://sakitibi-com9.webnode.jp/page/10";
 
 client.once("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
-  listenSupabaseChange(client, juusanninTermsURL);
-  listenSupabaseChangeWikis();
+    console.log(`Logged in as ${client.user.tag}`);
+    listenSupabaseChange(client, juusanninTermsURL);
+    listenSupabaseChangeWikis();
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
@@ -22,21 +21,26 @@ let NG_PATTERNS = loadNgPatterns();
 
 // メッセージ監視
 client.on("messageCreate", async (message) => {
-  if (message.author.bot) return;
+    if (message.author.bot) return;
 
-  const text = message.content;
+    const text = message.content;
 
-  // 正規表現で判定
-  const hit = NG_PATTERNS.find((regex) => regex.test(text));
+    // 正規表現で判定
+    const hit = NG_PATTERNS.find((regex) => regex.test(text));
 
-  if (hit) {
-    try {
-      await message.delete();
-      await message.channel.send(
-        `⚠️ ${message.author} パターン: \`${hit}\` は[13nin利用規約](${juusanninTermsURL})に違反しています`
-      );
-    } catch (err) {
-      console.error("NGワード処理エラー:", err);
+    if (hit) {
+        try {
+            await message.delete();
+            await message.channel.send(
+                `⚠️ ${message.author} パターン: \`${hit}\` は[13nin利用規約](${juusanninTermsURL})に違反しています`
+            );
+        } catch (err) {
+            console.error("NGワード処理エラー:", err);
+        }
     }
-  }
 });
+
+setInterval(() => {
+    listenSupabaseChange(client, juusanninTermsURL);
+    listenSupabaseChangeWikis();
+}, 1000 * 60 * 30);
